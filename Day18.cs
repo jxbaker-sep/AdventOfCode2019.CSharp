aa5e6730afaf77528225a6d3c2874ea76deb7dd6
Jake Baker <SEP.Baker@fronius.com>
2025-01-13 14:31:18 -0500

    wip

--- Day18.cs
+++ Day18.cs
@@ -22,9 +22,9 @@ public class Day18
   // [InlineData("Day18.Sample.1", 8)]
   // [InlineData("Day18.Sample.2", 86)]
   // [InlineData("Day18.Sample.3", 132)]
-  [InlineData("Day18.Sample.4", 136)]
+  // [InlineData("Day18.Sample.4", 136)]
   // [InlineData("Day18.Sample.5", 81)]
-  // [InlineData("Day18", 0)]
+  [InlineData("Day18", 0)]
   public void Part1(string path, long expected)
   {
     var grid = Convert(AoCLoader.LoadLines(path));
@@ -32,16 +32,16 @@ public class Day18
     CollectKeys(grid, start).Should().Be(expected);
   }
 
-  private static long CollectKeys(Dictionary<Point, char> grid, Point start)
+  private static long CollectKeys_old(Dictionary<Point, char> grid, Point start)
   {
     var allKeys = grid.Where(it => IsKey(it.Value)).Select(it => (position: it.Key, key: it.Value)).ToList();
     var goal = allKeys.Count;
     long salt(Point position, HashSet<char> k2) { 
-      return goal - k2.Count;
-      // var remainder = allKeys.Where(k3 => !k2.Contains(k3.key)).Select(it => it.position).ToList();
-      // if (remainder.Count == 0) return 0;
-      // if (remainder.Count == 1) return position.ManhattanDistance(remainder[0]);
-      // return remainder.Pairs().Min(pair => pair.First.ManhattanDistance(pair.Second) + Math.Min(position.ManhattanDistance(pair.First), position.ManhattanDistance(pair.Second)));
+      // return goal - k2.Count;
+      var remainder = allKeys.Where(k3 => !k2.Contains(k3.key)).Select(it => it.position).ToList();
+      if (remainder.Count == 0) return 0;
+      if (remainder.Count == 1) return position.ManhattanDistance(remainder[0]);
+      return remainder.Pairs().Min(pair => pair.First.ManhattanDistance(pair.Second) + Math.Min(position.ManhattanDistance(pair.First), position.ManhattanDistance(pair.Second)));
     }
     PriorityQueue<(long steps, HashSet<char> keys, Point position)> open = new((it) => it.steps + salt(it.position, it.keys));
     open.Enqueue((0, [], start));
@@ -49,10 +49,10 @@ public class Day18
     while (open.TryDequeue(out var current))
     {
       if (current.keys.Count == goal) return current.steps;
-      // if (closed.TryGetValue(current.position, out var cached2))
-      // {
-      //   if (cached2.Any(item => current.keys.All(ck => item.keys.Contains(ck)) && item.steps < current.steps)) continue;
-      // }
+      if (visited.TryGetValue(current.position, out var cached2))
+      {
+        if (cached2.Any(item => current.keys.All(ck => item.keys.Contains(ck)) && item.steps < current.steps)) continue;
+      }
       foreach(var (steps, key, nextPosition) in Open(grid, current.position, current.keys)) {
         HashSet<char> nextKeys = [..current.keys, key];
         var nextSteps = current.steps + steps;
