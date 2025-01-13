5b97e943a3e53e41074526aae07984645c36db08
Jake Baker <SEP.Baker@fronius.com>
2025-01-13 10:38:59 -0500

    day16.2

--- Day16.cs
+++ Day16.cs
@@ -10,20 +10,6 @@ namespace AdventOfCode2018.CSharp;
 
 public class Day16
 {
-  public Day16()
-  {
-    FFT2 = Cached.Create((int index, long phases) =>
-    {
-      if (phases == 0) return data[index];
-      long result = 0;
-      foreach (var (First, Second) in Pattern(index + 1).Take(data.Count).Zip(data.Select((_, dx) => dx)))
-      {
-        if (First == 0) continue;
-        result += FFT2!(Second, phases - 1) * First;
-      }
-      return Math.Abs((result % 10));
-    });
-  }
 
   [Theory]
   [InlineData("Day16", "30550349")]
@@ -31,6 +17,29 @@ public class Day16
   {
     var data = Convert(AoCLoader.LoadFile(path));
     FFT(data, 100).Take(8).Join().Should().Be(expected);
+  }
+
+  [Theory]
+  [InlineData("Day16", "62938399")]
+  public void Part2(string path, string expected)
+  {
+    var data = Convert(AoCLoader.LoadFile(path));
+    var index = System.Convert.ToInt32(AoCLoader.LoadFile(path)[..7]);
+    data = Enumerable.Repeat(data, 10_000).SelectMany(it => it).ToList();
+    var temp = data[index..];
+    foreach(var i in Enumerable.Range(0, 100))
+    {
+      Stack<long> result = [];
+      long previous = 0;
+      foreach(var xxx in Enumerable.Range(0, temp.Count))
+      {
+        var index2 = temp.Count - 1 - xxx;
+        previous += temp[index2];
+        result.Push(Math.Abs(previous) % 10);
+      }
+      temp = [.. result];
+    }
+    temp.Take(8).Join().Should().Be(expected);
   }
 
   [Theory]
@@ -45,41 +54,30 @@ public class Day16
     FFT(data, phases).Take(8).Join().Should().Be(expected);
   }
 
-  List<long> data = [];
-  [Theory]
-  [InlineData("12345678", 1, "48226158")]
-  [InlineData("12345678", 4, "01029498")]
-  [InlineData("80871224585914546619083218645595", 100, "24176176")]
-  [InlineData("19617804207202209144916044189917", 100, "73745418")]
-  [InlineData("69317163492948606335995924319873", 100, "52432133")]
-  public void Temp(string input, int phases, string expected)
-  {
-    data = Convert(input);
-    Enumerable.Range(0, 8).Select(it => FFT2(it, phases)).Join().Should().Be(expected);
-  }
-
-  public Func<int, long, long> FFT2 { get; }
 
   [Theory]
-  // [InlineData("03036732577212944063491565474664", 100, "84462026")]
+  [InlineData("03036732577212944063491565474664", 100, "84462026")]
   [InlineData("02935109699940807407585447034323", 100, "78725270")]
-  // [InlineData("03081770884921959731165446850517", 100, "53553731")]
+  [InlineData("03081770884921959731165446850517", 100, "53553731")]
   public void Sanity2(string input, int phases, string expected)
   {
-    data = Convert(input);
+    var data = Convert(input);
     data = Enumerable.Repeat(data, 10_000).SelectMany(it => it).ToList();
     var index = System.Convert.ToInt32(input[..7]);
     var temp = data[index..];
     foreach(var i in Enumerable.Range(0, phases))
     {
-      List<long> result = [];
+      Stack<long> result = [];
+      long previous = 0;
       foreach(var xxx in Enumerable.Range(0, temp.Count))
       {
-        result.Add(Math.Abs(temp[xxx..].Sum() % 10));
+        var index2 = temp.Count - 1 - xxx;
+        previous += temp[index2];
+        result.Push(Math.Abs(previous) % 10);
       }
-      temp = result;
+      temp = [.. result];
     }
-    // Enumerable.Range(index, 8).Select(it => FFT2(it, phases)).Join().Should().Be(expected);
+    temp.Take(8).Join().Should().Be(expected);
   }
 
   public static List<long> FFT(List<long> input, int phases)
