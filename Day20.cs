1f51927675d30123d005927771a8bf04ba2793cd
Jake Baker <SEP.Baker@fronius.com>
2025-01-16 13:07:29 -0500

    Refactored for inner/outer portals.

--- Day20.cs
+++ Day20.cs
@@ -14,17 +14,19 @@ public class Day20
     var maze = Convert(AoCLoader.LoadFile(path));
 
     Queue<Point> open = [];
-    open.Enqueue(maze.PortalExits["AA"].Single());
-    var goal = maze.PortalExits["ZZ"].Single();
+    var start = maze.PortalExits.Single(it => it.Value.Label == "AA").Key;
+    var goal = maze.PortalExits.Single(it => it.Value.Label == "ZZ").Key;
+    open.Enqueue(start);
     Dictionary<Point, long> closed = [];
-    closed[maze.PortalExits["AA"].Single()] = 0;
+    closed[start] = 0;
 
     while (open.TryDequeue(out var current))
     {
       var n = closed[current];
       var neighbors = current.CardinalNeighbors().ToList();
-      if (maze.PortalEntrances.TryGetValue(current, out var portal)) {
-        neighbors.AddRange(maze.PortalExits[portal].Where(it => it != current));
+      if (maze.PortalExits.TryGetValue(current, out var portal)) {
+        if (portal.InnerExit != Point.Zero) neighbors.Add(portal.InnerExit);
+        neighbors.Add(portal.OuterExit);
       }
       foreach(var next in neighbors)
       {
@@ -42,38 +44,67 @@ public class Day20
     throw new ApplicationException();
   }
 
-  public record Maze(HashSet<Point> OpenGrid, Dictionary<Point, string> PortalEntrances,
-    Dictionary<string, List<Point>> PortalExits);
+  public record Portal(string Label, bool IsOuter, Point OuterExit, Point InnerExit);
+  public record Maze(HashSet<Point> OpenGrid, Dictionary<Point, Portal> PortalExits);
 
   private static Maze Convert(string data) {
     var grid = data.Split("\n").ToList().Gridify();
 
-    Dictionary<Point, string> portalEntrances = [];
-
     HashSet<Point> openGrid = [];
+
+    Dictionary<string, Portal> portals = [];
     foreach(var (point, value) in grid)
     {
       if (value == '.') openGrid.Add(point);
       if (char.IsLetter(value))
       {
-        foreach(var (next, entrance) in new[]{ 
-            (Vector.South, Vector.South * 2), 
-            (Vector.South, Vector.North),
-            (Vector.East, Vector.East * 2), 
-            (Vector.East, Vector.West)}) {
+        foreach(var (next, entrance, lookDirection) in new[]{ 
+            (Vector.South, Vector.South * 2, Vector.North), 
+            (Vector.South, Vector.North, Vector.South),
+            (Vector.East, Vector.East * 2, Vector.West), 
+            (Vector.East, Vector.West, Vector.East)}) {
             if (grid.TryGetValue(point + next, out var cn) && char.IsLetter(cn) &&
                 grid.TryGetValue(point + entrance, out var cn2) && cn2 == '.')
             {
-              var portal = $"{value}{cn}";
-              portalEntrances[point + entrance] = portal;
+              var label = $"{value}{cn}";
+              var exit = point + entrance;
+              var n = point + lookDirection;
+              bool isOuter = false;
+              while (true)
+              {
+                if (!grid.TryGetValue(n, out var temp))
+                {
+                  isOuter = true; 
+                  break;
+                }
+                if (temp == '.' || temp == '#') break;
+                n += lookDirection;
+              }
+
+              var portal = portals.TryGetValue(label, out var ptemp) ? ptemp : new Portal(label, isOuter, Point.Zero, Point.Zero);
+              if (isOuter) portal = portal with { OuterExit = exit };
+              else portal = portal with { InnerExit = exit };
+              portals[label] = portal;
             }
         }
       }
     }
-    Dictionary<string, List<Point>> portalExits = portalEntrances
-      .GroupBy(it => it.Value, it => it.Key)
-      .ToDictionary(it => it.Key, it => it.ToList());
 
-    return new(openGrid, portalEntrances, portalExits);
+    Dictionary<Point, Portal> portalExits = [];
+    foreach(var portal in portals.Values)
+    {
+      if (portal.InnerExit == Point.Zero && (portal.Label != "AA" && portal.Label != "ZZ"))
+      {
+        throw new ApplicationException();
+      }
+      if (portal.OuterExit == Point.Zero) throw new ApplicationException();
+      portalExits[portal.OuterExit] = portal;
+      if (portal.Label != "AA" && portal.Label != "ZZ")
+      {
+        portalExits[portal.InnerExit] = portal;
+      }
+    }
+
+    return new(openGrid, portalExits);
   }
 }
